af1cacbd67c8cd2aa91852c654879fb5fec5183a
aptustin <atustin@calpoly.edu>
2018-09-21 10:14:07 -0700

    Second version of planets.py Seems to be working properly in conjunction with planets_test.py

--- planets.py
+++ planets.py
@@ -1,16 +1,18 @@
 def weight_on_planets():
-   # write your code here
+    # write your code here
 
-   e_weight = input("What is your weight on earth?")
+    e_weight = input("What do you weigh on earth? ")
 
-   e_weight = float(e_weight)
+    # The following three lines casts the input as a float and converts the Earth weight to both Mars and Jupiter weight.
+    e_weight = float(e_weight)
 
-   m_weight = e_weight*.38
+    m_weight = e_weight*.38
 
-   j_weight = e_weight*2.34
+    j_weight = e_weight*2.34
 
-print "\nOn Mars you would weigh " + m_weight + " pounds.\nOn Jupiter you would weigh " + j_weight + " pounds."
+    # Prints out the calculations performed above.
+    print("\nOn Mars you would weigh", m_weight, "pounds.\nOn Jupiter you would weigh", j_weight,  "pounds.")
 
 
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
